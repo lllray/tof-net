26bb84265ffbeee65b8e7d2aa85b824ab2e34fcb
lixin <852701767@qq.com>
2022-04-20 03:19:10 +0800

    change my data success, now can use

--- scripts/show.m
+++ scripts/show.m
@@ -2,14 +2,14 @@ clc;clear;close all;
 % load('~/ROS/DeepEnd2End/DeepToF_release_0.1/src/GAN/results/my_resnet_lite_my_imageGAN_128_pretrained/latest_test/images/fake_B/000001.mat') %results are stored as .mat files under [results_folder]/fake_B
 % load('~/ROS/DeepEnd2End/DeepToF_release_0.1/src/GAN/results/my_resnet_lite_my_imageGAN_128_pretrained/latest_test_real_all_calib/images/fake_B/1110_pix2pix_test_real_5.mat') %results are stored as .mat files under [results_folder]/fake_B
 data_type = 2; % 1 = tintin 2 = my
-type = 'real_B'; % fake_B,real_A,real_B,sra
+type = 'fake_B'; % fake_B,real_A,real_B,sra
 if data_type == 1
 mydata = '1110';
 take = 5;
 % mydata = '0926';
 % take = 40;
 elseif data_type == 2
-mydata = '0419-1';
+mydata = '0420-3';
 end
 
 
@@ -17,8 +17,8 @@ if data_type == 1
     load(sprintf('~/ROS/DeepEnd2End/DeepToF_release_0.1/src/GAN/results/my_resnet_lite_my_imageGAN_128_pretrained/latest_test_real_all_calib/images/%s/%s_pix2pix_test_real_%d.mat',type,mydata,take)) %results are stored as .mat files under [results_folder]/fake_B
     gt = single(imread(sprintf('~/bag/tintin_EE367/data_tintin/meas_%s/phase_calibrated_norm2amp_rebuttal_mean5/%s_depth_%d.png',mydata,mydata,take)))/1e4;
 elseif data_type == 2
-    load(sprintf('~/ROS/DeepEnd2End/DeepToF_release_0.1/src/GAN/results/my_resnet_lite_my_imageGAN_128_pretrained/latest_test/images/%s/%s_pix2pix_test_real_0.mat',type,mydata)) %results are stored as .mat files under [results_folder]/fake_B
-    gt = single(imread(sprintf('~/bag/tintin_EE367/my_data/%s/phase_calibrated_norm2amp_rebuttal_mean5/%s_depth_0.png',mydata,mydata)))/1e4;
+    load(sprintf('~/ROS/DeepEnd2End/DeepToF_release_0.1/src/GAN/results/my_resnet_lite_my_imageGAN_128_pretrained/latest_test_real_all_calib/images/%s/%s_pix2pix_test_real_0.mat',type,mydata)) %results are stored as .mat files under [results_folder]/fake_B
+    gt = single(imread(sprintf('~/bag/tintin_EE367/my_data/%s/phase_calibrated_norm2amp_rebuttal_mean1/%s_depth_0.png',mydata,mydata)))/1e4;
 end
 if strcmp(type,'sra') == 1
     result = distSRA
@@ -48,18 +48,12 @@ subplot(121); pcshow(reshape(pointCloud_gt,320*240,3)); title('point cloud of gt
 subplot(122); pcshow(reshape(pointCloud_this,320*240,3)); title('point cloud of this depth');
 
 elseif data_type == 2
-    if strcmp(type,'sra') == 1
+    result = imresize(int32((result).*100),[180,240],'bilinear');
+    result = double(result)./100;
     pointCloud_gt = my_depthToPointCloud(gt,'no_calib');
-    pointCloud_this = my_depthToPointCloud(result);
+    pointCloud_this = my_depthToPointCloud(result,'no_calib');
     figure(2);
     subplot(121); pcshow(reshape(pointCloud_gt,240*180,3)); title('point cloud of gt depth');
     subplot(122); pcshow(reshape(pointCloud_this,240*180,3)); title('point cloud of this depth');
-    else
-    pointCloud_gt = my_depthToPointCloud(gt,'no_calib');
-    pointCloud_this = depthToPointCloud(result);
-    figure(2);
-    subplot(121); pcshow(reshape(pointCloud_gt,240*180,3)); title('point cloud of gt depth');
-    subplot(122); pcshow(reshape(pointCloud_this,320*240,3)); title('point cloud of this depth');    
-    end
 end
 
